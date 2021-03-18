852d00b9d2af36b9f5787ad009fed06114c90e09
Maria <alece.1414@gmail.com>
2021-03-18 23:37:36 +0300

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-let dayOptions = '<option value="day" class="question__option">День</option>';
+let dayOptions = '<option value="">День</option>';
 const daySelect = document.querySelector('#day');
 
 for (let i = 1; i <= 31; i++) {
-    dayOptions += `<option value="${i}" class="question__option">${i}</option>`;
+    dayOptions += `<option value="${i}">${i}</option>`;
 }
 
 daySelect.innerHTML = dayOptions;
 
 
-let monthOptions = '<option value="month">Месяц</option>';
+let monthOptions = '<option value="">Месяц</option>';
 const monthSelect = document.querySelector('#month');
 const months = ['Январь', 'Ферваль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
@@ -19,7 +19,7 @@ for (let i = 0; i < months.length; i++) {
 monthSelect.innerHTML = monthOptions;
 
 
-let yearOptions = '<option value="year">Год</option>';
+let yearOptions = '<option value="">Год</option>';
 const yearSelect = document.querySelector('#year');
 
 for (let i = 1940; i <= 2021; i++) {
